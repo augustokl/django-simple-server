d0ce3f25447149cf8f4671bccd2353eb32a83372
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-03-22 22:09:00 -0300

    initial commit django sample

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/augustokl/Documents/PythonLessons/django-lessons/profiles-rest-api/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
